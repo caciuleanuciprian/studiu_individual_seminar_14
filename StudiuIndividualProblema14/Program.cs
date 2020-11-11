ed685264dde9203b515c007810a6af565590acdc
Ciprian <56521866+caciuleanuciprian@users.noreply.github.com>
2020-11-11 21:34:45 +0200

    Add files via upload

---
+++ StudiuIndividualProblema14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudiuIndividualProblema14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, suma = 0;
+
+            Console.WriteLine("n= ");
+            n = int.Parse(Console.ReadLine());
+
+            while(n != 0)
+            {
+                suma = suma + (n % 10);
+                n = n / 10;
+            }
+
+            Console.WriteLine(suma);
+
+            Console.ReadLine();
+
+        }
+    }
+}
